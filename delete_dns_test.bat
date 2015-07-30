7904ce94dfc9d8090220a3eb165ed218a2e3e550
rensq <rsqmail@163.com>
2015-07-30 20:55:51 +0800

    add dns_test_tool

---
+++ delete_dns_test.bat
@@ -0,0 +1,3 @@
+%cd%
+adb shell "cd /sdcard/Android/data/com.tencent.killer/files;rm dns_test"
+pause
---
+++ touch_dns_test.bat
@@ -0,0 +1,3 @@
+%cd%
+adb shell "cd /sdcard/Android/data/com.tencent.killer/files;touch dns_test"
+pause
